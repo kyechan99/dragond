e8695b68d15caa9869b5992b56334d9c728a090e
kyechan99 <kyechan99@gmail.com>
2021-02-09 18:55:41 +0900

    Init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <h2>Essential Links</h2>
+    <h2>Essential Links</h2>
+    <h2>Essential Links</h2>
+    <h2>Essential Links</h2>
+    <dragond></dragond>
+    <div id="console"></div>
+  </div>
+</template>
+
+<script>
+import dragond from '@/components/dragond'
+
+export default {
+  components: { dragond },
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+function selectText () {
+  var selectionText = ''
+  if (document.getSelection) {
+    selectionText = document.getSelection()
+  } else if (document.selection) {
+    selectionText = document.selection.createRange().text
+  }
+  return selectionText
+}
+
+document.onmouseup = function () {
+  document.getElementById('console').innerHTML = selectText()
+}
+
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/dragond.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dragond">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dragond',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
