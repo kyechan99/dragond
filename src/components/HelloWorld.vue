6c0a011ee64661e14638037efa87d47c395dae78
kyechan99 <kyechan99@gmail.com>
2021-02-10 23:19:25 +0900

    Divide Component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <h2>Essential Links</h2>
-    <h2>Essential Links</h2>
-    <h2>Essential Links</h2>
-    <h2>Essential Links</h2>
-    <dragond></dragond>
+  <div>
+    <h1>Essential Links</h1>
+    <dragond @dragondData="dragondData">
+      <h2>Essential Links</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <h2>Essential Links</h2>
+      <h2>Essential Links</h2>
+      <h2>Essential Links</h2>
+    </dragond>
     <div id="console"></div>
+    {{ msg }}
   </div>
 </template>
 
@@ -19,23 +21,15 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: ''
+    }
+  },
+  methods: {
+    dragondData: function (data) {
+      console.log(data)
+      this.msg = data
     }
   }
-}
-
-function selectText () {
-  var selectionText = ''
-  if (document.getSelection) {
-    selectionText = document.getSelection()
-  } else if (document.selection) {
-    selectionText = document.selection.createRange().text
-  }
-  return selectionText
-}
-
-document.onmouseup = function () {
-  document.getElementById('console').innerHTML = selectText()
 }
 
 </script>
--- src/components/dragond.vue
+++ src/components/dragond.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="dragond">
+  <div class="dragond" @mouseup="mouseup">
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
   name: 'dragond',
+  props: {
+
+  },
   data () {
     return {
     }
+  },
+  methods: {
+    selectText: function () {
+      return window.getSelection().toString()
+    },
+    mouseup: function () {
+      this.$emit('dragondData', this.selectText())
+    }
   }
 }
+
 </script>
 
 <style scoped>
